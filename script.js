82965f3059cacd4e5760c54fa219cadd496188ee
David Keller <mrcodeswildride@gmail.com>
2020-06-16 14:47:33 +0000

    es6 update

--- script.js
+++ script.js
@@ -1,56 +1,56 @@
-var itemInput = document.getElementById("item");
-var categoryInput = document.getElementById("category");
-var addItemButton = document.getElementById("addItem");
-var categoriesDiv = document.getElementById("categories");
+let item = document.getElementById(`item`)
+let category = document.getElementById(`category`)
+let addButton = document.getElementById(`addButton`)
+let inventory = document.getElementById(`inventory`)
 
-var categories = {};
+let categories = {}
 
-addItemButton.addEventListener("click", addItem);
+addButton.addEventListener(`click`, addItem)
 
 function addItem() {
-    var itemName = itemInput.value.trim();
-    var categoryName = categoryInput.value.trim();
+  let itemName = item.value.trim()
+  let categoryName = category.value.trim()
 
-    if (itemName != "" && categoryName != "") {
-        if (categories[categoryName] == undefined) {
-            categories[categoryName] = {};
-        }
-
-        if (categories[categoryName][itemName] == undefined) {
-            categories[categoryName][itemName] = 0;
-        }
-
-        categories[categoryName][itemName]++;
-
-        categoriesDiv.innerHTML = "";
-        showCategories();
+  if (itemName != `` && categoryName != ``) {
+    if (categories[categoryName] == null) {
+      categories[categoryName] = {}
     }
+
+    if (categories[categoryName][itemName] == null) {
+      categories[categoryName][itemName] = 0
+    }
+
+    categories[categoryName][itemName]++
+
+    inventory.innerHTML = ``
+    showCategories()
+  }
 }
 
 function showCategories() {
-    for (var categoryName in categories) {
-        var categoryHeading = document.createElement("h2");
-        categoryHeading.innerHTML = categoryName;
-        categoriesDiv.appendChild(categoryHeading);
+  for (let categoryName in categories) {
+    let categoryHeading = document.createElement(`h2`)
+    categoryHeading.innerHTML = categoryName
+    inventory.appendChild(categoryHeading)
 
-        showItems(categories[categoryName]);
-    }
+    showItems(categories[categoryName])
+  }
 }
 
 function showItems(items) {
-    for (var itemName in items) {
-        var itemDiv = document.createElement("div");
-        itemDiv.classList.add("item");
-        categoriesDiv.appendChild(itemDiv);
+  for (let itemName in items) {
+    let itemDiv = document.createElement(`div`)
+    itemDiv.classList.add(`item`)
+    inventory.appendChild(itemDiv)
 
-        var itemCountDiv = document.createElement("div");
-        itemCountDiv.classList.add("itemCount");
-        itemCountDiv.innerHTML = items[itemName];
-        itemDiv.appendChild(itemCountDiv);
+    let itemCountDiv = document.createElement(`div`)
+    itemCountDiv.classList.add(`itemCount`)
+    itemCountDiv.innerHTML = items[itemName]
+    itemDiv.appendChild(itemCountDiv)
 
-        var itemNameDiv = document.createElement("div");
-        itemNameDiv.classList.add("itemName");
-        itemNameDiv.innerHTML = itemName;
-        itemDiv.appendChild(itemNameDiv);
-    }
+    let itemNameDiv = document.createElement(`div`)
+    itemNameDiv.classList.add(`itemName`)
+    itemNameDiv.innerHTML = itemName
+    itemDiv.appendChild(itemNameDiv)
+  }
 }
